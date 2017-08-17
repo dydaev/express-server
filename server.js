9da20b5d23eb8585961fad7571c029a603bd0ce5
dydaev <dydaev@gmail.com>
2017-08-17 19:14:29 +0300

    first test, create three pages

--- server.js
+++ server.js
@@ -5,11 +5,38 @@ var port = 8080;
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/build'));
+//app.use(express.static(__dirname + '/build'));
+
+/*const pages = {[
+	{home: {
+		visible: true,
+		uri: "/"
+	}},
+	{about: {
+		visible: true,
+		uri: "/about"
+	}},
+	{contact: {
+		visible: true,
+		uri: "/contact"
+	}},
+	{found404: {
+		visible: false,
+		uri: "/notFound.html"
+	}}
+]};*/
 
 app.get('/', function(req, res) {
-    res.render('test express server');
+    res.render('index');
 });
+
+app.get('/about', (req, res) => {
+	res.render('about');
+})
+
+app.get('/contact', (req, res) => {
+	res.render('contact');
+})
 
 app.listen(port, function() {
     console.log('Express server started to http://localhost:' + port);
